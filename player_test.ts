56619e906c33d3dfe9b3fefdf2c73f364ee39a5b
Qiang Ji <qiang.j@gmail.com>
2019-09-07 16:55:11 +1000

    format code

--- player_test.ts
+++ player_test.ts
@@ -1,15 +1,18 @@
 import { runTests, test } from "https://deno.land/std@v0.17.0/testing/mod.ts";
-import { assertEquals, assertArrayContains } from "https://deno.land/std@v0.17.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertArrayContains
+} from "https://deno.land/std@v0.17.0/testing/asserts.ts";
 import Player from "./player.ts";
 
 test({
-    name: "test player",
-    fn(): void {
-      const name: string = "Glenn";
-      const player = new Player(name);
-      assertEquals(player.name, name);
-      assertArrayContains(player.cards, []);
-    }
-  });
-  
-  runTests();
+  name: "test player",
+  fn(): void {
+    const name: string = "Glenn";
+    const player = new Player(name);
+    assertEquals(player.name, name);
+    assertArrayContains(player.cards, []);
+  }
+});
+
+runTests();
