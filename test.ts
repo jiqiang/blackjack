47d56129f9b2d07af91d9545b905831e528e23d1
Qiang Ji <jiqiang@Qiangs-MacBook-Pro.local>
2019-09-07 11:33:10 +1000

    update dependencies version

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import { runTests, test } from "https://deno.land/std@v0.16.0/testing/mod.ts";
-import { assertEquals } from "https://deno.land/std@v0.16.0/testing/asserts.ts";
+import { runTests, test } from "https://deno.land/std@v0.17.0/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std@v0.17.0/testing/asserts.ts";
 import { calculateHandValues } from './bj.ts';
 
 test({
